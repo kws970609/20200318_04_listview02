6d5cc579c4d4d22c4f88b4d14fac924beac5e9a2
kws970609 <kws970609@users.noreply.github.com>
2020-03-18 15:40:00 +0900

    메인 액티비티에서 어댑터/목록 변수 생성 +리스트 뷰와 연결

--- app/src/main/java/kr/co/tjoeun/a20200318_04_listview02/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/a20200318_04_listview02/MainActivity.java
@@ -5,9 +5,16 @@ import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import kr.co.tjoeun.a20200318_04_listview02.adapters.RoomAdater;
 import kr.co.tjoeun.a20200318_04_listview02.databinding.ActivityMainBinding;
+import kr.co.tjoeun.a20200318_04_listview02.datas.Room;
 
 public class MainActivity extends BaseActivity {
+    List<Room> roomDatas = new ArrayList<>();
+    RoomAdater roomAdater = null;
 
     ActivityMainBinding binding = null;
 
@@ -26,6 +33,7 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setValues() {
-
+        roomAdater = new RoomAdater(mContext, R.layout.room_list_item, roomDatas);
+        binding.roomListView.setAdapter(roomAdater);
     }
 }
